021413fdec97d986276fad4334fa086b5cbaec3c
bvrevanth <revanth47.2017@gmail.com>
2024-07-15 23:08:25 +0530

    day 2 completed

---
+++ day-2.js
@@ -0,0 +1,137 @@
+// Day - 2 OPERATORS //
+
+// Activity - 1
+
+// Task - 1
+
+let a = 3;
+let b = 4;
+let add = a + b;
+// console.log(add);
+
+// Task - 2
+
+let c = 5;
+let d = 4;
+let sub = c - d;
+// console.log(sub);
+
+// Task - 3
+
+let e = 2;
+let f = 2;
+let mul = e * f;
+// console.log(mul);
+
+// Task - 4
+
+let g = 4;
+let h = 2;
+let div = g / h;
+// console.log(div);
+
+// Task - 5
+
+let i = 6;
+let j = 3;
+let rem = i % j;
+// console.log(rem);
+
+// Activity - 2
+
+// Task - 6
+
+let num1 = 5;
+num1 += 1;
+// console.log(num1);
+// o/p : 6
+
+// Task - 7
+
+let num2 = 5;
+num2 -= 1;
+// console.log(num2);
+// o/p : 4
+
+// Activity - 3
+
+// Task - 8,9,10
+
+let new1 = 8;
+let new2 = 9;
+
+// console.log(new1 > new2);
+// o/p : False
+// console.log(new1 < new2);
+// o/p : True
+// console.log(new1 >= new2);
+// o/p : False
+// console.log(new1 <= new2);
+// o/p : True
+// console.log(new1 == new2);
+// o/p : False
+// console.log(new1 === new2);
+// o/p : False
+
+// let age = 22;
+
+// if (age > 18) {
+//   console.log("You are major");
+// } else {
+//   console.log("you are not major");
+// }
+
+// if (age < 18) {
+//   console.log("you are minor");
+// } else {
+//   console.log("you are not minor");
+// }
+
+// if (age >= 18) {
+//   console.log("you are major & eligible to vote");
+// } else if (age <= 18) {
+//   console.log("you are minor & unable to vote");
+// }
+
+// let age2 = "18";
+
+// if (age2 == 18) {
+//   console.log("congrats , you are major");
+// } else {
+//   console.log("you are not");
+// }
+
+// if (age2 === "18") {
+//   console.log("congrats , you are major");
+// } else {
+//   console.log("you are not");
+// }
+
+// Activity - 4
+// Task - 11,12,13
+
+// let teen = 16;
+
+// if (teen > 12 && teen < 20) {
+//   console.log("You are teenager");
+// } else {
+//   console.log("you are not teenager");
+// }
+
+// if (teen > 12 || teen < 20) {
+//   console.log("You are teenager");
+// } else {
+//   console.log("you are not teenager");
+// }
+
+// let major = true;
+// console.log(!major);
+
+// Activity - 5
+
+// Task - 14
+
+// let test = -5;
+let test = 5;
+
+console.log(test > 0 ? "The number is positive" : "The number is negative");
