69bd78a432c7e3277ec083f89fa7b58340471764
bvrevanth <revanth47.2017@gmail.com>
2024-07-15 18:00:35 +0530

    day 1 assignment

---
+++ day-1.js
@@ -0,0 +1,82 @@
+// Activity - 1 : Variable declaration
+
+// Task - 1
+
+var num = 7;
+console.log(num);
+// o/p: 7
+
+// Task - 2
+
+let str = "chai";
+console.log(str);
+// o/p: chai
+
+// Activity - 2
+
+// Task - 3
+
+const boo = true;
+console.log(boo);
+
+// Activity - 3
+
+// Task - 4
+
+var numb = 7;
+var stri = "Code";
+var bool = false;
+var bool2 = true;
+var obj = { name: "Revanth", age: 28 };
+var arr = ["revanth", 28, true];
+
+// let numb = 7;
+// let stri = "Code";
+// let bool = false;
+// let bool2 = true;
+// let obj = { name: "Revanth", age: 28 };
+// let arr = ["revanth", 28, true];
+
+// const numb = 7;
+// const stri = "Code";
+// const bool = false;
+// const bool2 = true;
+// const obj = { name: "Revanth", age: 28 };
+// const arr = ["revanth", 28, true];
+
+console.log(numb, "Type of variable is " + typeof numb);
+// o/p : number;
+console.log(stri, "Type of variable is " + typeof stri);
+// o/p : string;
+console.log(bool, "Type of variable is " + typeof bool);
+// o/p : boolean;
+console.log(bool2, "Type of variable is " + typeof bool2);
+// o/p : boolean;
+console.log(obj, "Type of variable is " + typeof obj);
+// o/p : object;
+console.log(arr, "Type of variable is " + typeof arr);
+// o/p: object;
+
+// Note : type of an "array" = "object"
+
+// Activity - 4
+
+// Task - 5
+
+let city = "Hyderabad";
+console.log(city);
+// o/p: Hyderabad
+city = "Vizag";
+console.log(city);
+// o/p: Vizag
+
+// Activity - 5
+
+// Task - 6
+
+const nameIs = "Ramesh";
+console.log(nameIs);
+// o/p: Ramesh
+nameIs = "Pavan";
+console.log(nameIs);
+// o/p: Error ( type error )
